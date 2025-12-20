fd388223904408f8cf6c2f90d211762703e75880
Cubitfroth <cubit_froths0o@icloud.com>
2025-12-21 00:43:40 +0300

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -1,11 +1,19 @@
 ## This is the *library* that translates html to typst
 
 sed -i 's/<title>/=/g' output.txt
-
-
-
-
-
-## final deletion
+sed -i 's/<h1>/== /g' output.txt
+sed -i 's/<\/h1>//g' output.txt
+sed -i 's/<h2>/=== /g' output.txt
+sed -i 's/<\/h2>//g' output.txt
+sed -i 's/<h3>/==== /g' output.txt
+sed -i 's/<\/h3>//g' output.txt
+sed -i 's/<p>/\n/g' output.txt
+sed -i 's/<\/p>/\n/g' output.txt
+sed -i 's/<strong>/**/g' output.txt
+sed -i 's/<\/strong>/**/g' output.txt
+sed -i 's/<em>/_/g' output.txt
+sed -i 's/<\/em>/_/g' output.txt
+sed -i 's/<a href="\(.*\)">/\[\1]/g' output.txt
+sed -i 's/<\/a>/\[\]/g' output.txt
 sed -i 's/<[^>]*>//g' output.txt
 
